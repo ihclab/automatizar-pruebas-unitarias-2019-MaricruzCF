7c519a527726d31ed8877532f27fa40c1d8eb0f7
Maricruz Michel <mmichel5@ucol.mx>
2019-03-30 20:01:57 -0600

    correccion de problema 1 y 2

--- Medias.cs
+++ Medias.cs
@@ -11,21 +11,41 @@ namespace AutomatizarPruebasUnitarias {
         /**
          * Calcula y regresa la media artimética
          */
-        public static double mediaAritmetica(params int[] vals) { }
+        public static double mediaAritmetica(params int[] vals) { 
+            int suma = 0;
+            for(int i= 0; i<vals.Length;i++){
+                suma += vals[i];
+            }
+            return suma/vals.Length;
+        }
 
         /**
          * Calcula y regresa la raíz enésima = x^(1/n)
          */
-        private static double raizEnesima(double x, int n) { }
+        private static double raizEnesima(double x, int n) { 
+            return Math.Pow(x,1/n);
+        }
 
         /**
          *  Usa raizEnesima para calcular y regresar la media geométrica
          */
-        public double mediaGeometrica(params int[] vals) { }
+        public double mediaGeometrica(params int[] vals) { 
+            int multi;
+            for(int i = 0;i<vals.Length;i++){
+                multi *= vals[i];
+            }
+            return raizEnesima(multi, vals.Length);
+        }
 
         /**
          * Este método no está implementado.
          */
-        public static double mediaArmonica(params int[] vals) { }
+        public static double mediaArmonica(params int[] vals) {
+            double suma =0;
+            for(int i = 0; i <vals.Length; i++){
+                suma += 1 / vals[i];
+            }
+            return vals.Length/suma;
+         }
     }
 }
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AutomatizarPruebasUnitarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] casos = File.ReadAllLines("CasosPrueba.txt");
+            
+
+        }
+    }
+}
